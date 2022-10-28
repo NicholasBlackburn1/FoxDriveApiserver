b04a1ac596fc2d21e84c22d708324d28ed2f8f1d
NicholasBlackburn1 <nickblackburn02@gmail.com>
2022-10-28 19:56:36 -0400

    setting u p the db for the usrs

--- foxdriveapiserver/api/routes.py
+++ foxdriveapiserver/api/routes.py
@@ -27,7 +27,7 @@ from flask import (
 from datetime import datetime
 import pathlib
 from utils import Consts, logger
-
+import apps as app
 # swagger
 swaggeryamls = str(pathlib.Path().absolute()) + "/documents/swagger/"
 
@@ -52,5 +52,25 @@ creates and manages users in the db
 @apibp.route(Consts.users+"create", methods=["POST"])
 
 def createuser():
-    logger.info("Creating New User")
+
+    logger.warning(" got request to make new user.....")
+    logger.info(" got new user info...")
+
+    user = app.users(**request.json)
+
+    # users db
+    user.name = request.json["name"]
+    user.userid = request.json["userid"]
+    user.create_date = request.json["create_date"]
+
+
+    if (
+        app.db.session.query(app.users).filter_by(name=user.name).scalar()
+        is not None
+    ):
+       
+        logger.Error("Entry exsits wont create a new one!")
+        return jsonify({"status": "usr in db already", "user": str(user.name)})
+       
+
 
